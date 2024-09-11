e064dd439b4618d5c3a3be4d4103adf1f2b76f75
YoussefProDev <youssef23468210@gmail.com>
2024-09-11 19:07:13 +0200

    initial Commit

---
+++ src/constants/CommonCurrency.ts
@@ -0,0 +1,5 @@
+// import data from './CommonCurrency.json'; // Importa il JSON
+// import { CurrencyData } from '../types'; // Importa l'interfaccia
+
+// export const Currencies: CurrencyData = data;
+
